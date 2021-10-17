e5bdfc7dd03e705fa669bc49fef743c84397809b
Esther <esther.choi@etu.sorbonne-universite.fr>
2021-10-17 19:06:33 +0200

    tests question 3.2

--- test.py
+++ test.py
@@ -1,31 +1,60 @@
 from src import *
+from time import time
 
-G = createGraphFromTxt('exempleinstance2.txt')
-# draw_graph(G)
-
-""" H = delete_node(G,0)
-draw_graph(H) """
-
-""" H = delete_list_node(G,[0,1])
-draw_graph(H) """
-
-""" d = degrees(G)
-print(d) """
-
-""" maxd = max_degree(G)
-print(maxd) """
-
-""" draw_graph(generate_random_graph(25,0.3)) """
-
-""" print(G.edges)
-print(algo_couplage(G)) """
-
-""" print(algo_glouton(G)) """
+""" G = parser('exempleinstance2.txt')
+# draw_graph(G) """
 
 """ print(branch(G))
 print(branch2(G))
 draw_graph(G) """
 
-print(branch(G))
+""" print(branch(G))
 print(branch3(G))
-draw_graph(G)
+draw_graph(G) """
+
+### TESTS 3.2 : ALGO_COUPLAGE ET ALGO_GLOUTON
+
+N_max = 600
+p = 1
+print("N_max =", N_max)
+print("p =", p)
+
+for k in range(1,11):
+    n = int(k*N_max/10)
+
+    tn_generation = 0.0
+    tn_couplage = 0.0
+    tn_glouton = 0.0
+
+    print("n =", n)
+
+    for i in range(10):
+
+        t1 = time()
+        G = generate_random_graph(n,p)
+        t2 = time()
+        tn_generation += (t2-t1)
+
+        #algo_couplage
+        t1 = time()
+        algo_couplage(G)
+        t2 = time()
+        tn_couplage += (t2-t1)
+        
+
+        t1 = time()
+        algo_glouton(G)
+        t2 = time()
+        tn_glouton += (t2-t1)
+
+    # calcul de la moyenne des temps d'exécution
+    tn_generation = tn_generation/10
+    tn_couplage = tn_couplage/10
+    tn_glouton = tn_glouton/10
+
+    print("\tgeneration du graphe :", tn_generation)
+    print("\t\t log :", np.log(tn_generation))
+    print("\talgo_couplage :", tn_couplage)
+    print("\t\t log :", np.log(tn_couplage))
+    print("\talgo_glouton :", tn_glouton)
+    print("\t\t log :", np.log(tn_glouton))
